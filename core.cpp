8a5392002c5dbd8aa0b4ae608a13ebd4f85d0915
Thomas Saint-Gérand <devspaceship@gmail.com>
2022-05-07 16:03:06 +0200

    Changed param naming

--- core.cpp
+++ core.cpp
@@ -3,18 +3,18 @@
 
 int main(int argc, char *argv[])
 {
-    float x_a = -2;
-    float y_a = 2;
-    float x_b = 2;
-    float y_b = -2;
+    float x_min = -2;
+    float x_max = 2;
+    float y_min = -2;
+    float y_max = 2;
     int w = 1920;
     int h = 1080;
 
     CLI::App app{"Compute the number of iterations before leaving bounds"};
-    app.add_option("--x_a", x_a, "the x coordinate of the top left corner");
-    app.add_option("--y_a", y_a, "the y coordinate of the top left corner");
-    app.add_option("--x_b", x_b, "the x coordinate of the bottom right corner");
-    app.add_option("--y_b", y_b, "the y coordinate of the bottom right corner");
+    app.add_option("--x_min", x_min, "the minimum x coordinate");
+    app.add_option("--x_max", x_max, "the maximum x coordinate");
+    app.add_option("--y_min", y_min, "the minimum y coordinate");
+    app.add_option("--y_max", y_max, "the maximum y coordinate");
     app.add_option("--width", w, "the horizontal resolution of the computation");
     app.add_option("--height", h, "the vertical resolution of the computation");
     CLI11_PARSE(app, argc, argv);
